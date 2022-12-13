1b53beba409e2a204a3d45c29dbe2dfd7bc4b75f
JoshiPriya1122 <120475072+JoshiPriya1122@users.noreply.github.com>
2022-12-13 21:10:21 +0530

    phonex changes

---
+++ Soundex.java
@@ -0,0 +1,91 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Soundex {
+    public static String getGode(String s)
+    {
+        char[] x = s.toUpperCase().toCharArray();
+
+
+        char firstLetter = x[0];
+
+        //RULE [ 2 ]
+        //Convert letters to numeric code
+        for (int i = 0; i < x.length; i++) {
+            switch (x[i]) {
+                case 'B':
+                case 'F':
+                case 'P':
+                case 'V': {
+                    x[i] = '1';
+                    break;
+                }
+
+                case 'C':
+                case 'G':
+                case 'J':
+                case 'K':
+                case 'Q':
+                case 'S':
+                case 'X':
+                case 'Z': {
+                    x[i] = '2';
+                    break;
+                }
+
+                case 'D':
+                case 'T': {
+                    x[i] = '3';
+                    break;
+                }
+
+                case 'L': {
+                    x[i] = '4';
+                    break;
+                }
+
+                case 'M':
+                case 'N': {
+                    x[i] = '5';
+                    break;
+                }
+
+                case 'R': {
+                    x[i] = '6';
+                    break;
+                }
+
+                default: {
+                    x[i] = '0';
+                    break;
+                }
+            }
+        }
+
+        //Remove duplicates
+        //RULE [ 1 ]
+        String output = "" + firstLetter;
+
+        //RULE [ 3 ]
+        for (int i = 1; i < x.length; i++)
+            if (x[i] != x[i - 1] && x[i] != '0')
+                output += x[i];
+
+        //RULE [ 4 ]
+        //Pad with 0's or truncate
+        output = output + "0000";
+        return output.substring(0, 4);
+    }
+
+    public static void main(String[] args) {
+        String[] str= new String[]{"beer","bear","bearer"};
+
+        List<String> sameValues = Arrays.stream(str).map(s -> Soundex.getGode(s)).collect(Collectors.toList());
+        int bear = Collections.frequency(sameValues, Soundex.getGode("beer"));
+        System.out.println(bear);
+    }
+}
